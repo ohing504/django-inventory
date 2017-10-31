85201b94ccf359439bae86234df96b8642257320
youngsup.oh <youngsup.oh@h9works.com>
2017-10-31 14:31:22 +0900

    Fix django.jQuery is not a function at 'django-jet'

--- tripfo/tripfo/settings.py
+++ tripfo/tripfo/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'tripfo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
