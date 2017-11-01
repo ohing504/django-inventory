ce5f7a20b25a27da291e19f6dc7d2989178154ba
youngsup.oh <youngsup.oh@h9works.com>
2017-11-01 17:21:34 +0900

    Update travel model

--- tripfo/travel/models.py
+++ tripfo/travel/models.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Travel(models.Model):
     title = models.CharField('Title', max_length=127)
+    users = models.ManyToManyField(User, blank=True)
+    start_date = models.DateTimeField('Start Time', blank=True, null=True)
+    end_date = models.DateTimeField('End Time', blank=True, null=True)
+
+    created = models.DateTimeField('Created', auto_now_add=True)
+    updated = models.DateTimeField('Updated', auto_now=True)
 
     class Meta:
         verbose_name = 'Travel'
