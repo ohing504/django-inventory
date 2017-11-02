32046aa9831573e7dbebb6181622443b274a3dcc
youngsup.oh <youngsup.oh@h9works.com>
2017-11-02 15:25:27 +0900

    Add Whitenoise package for serving static assets

--- tripfo/settings.py
+++ tripfo/settings.py
@@ -143,6 +143,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, 'staticfiles'))
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Django REST Framework
 
